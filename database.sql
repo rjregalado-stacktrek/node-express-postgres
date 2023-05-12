abbdc0b18ce3636b38df41900e163da42a9f343e
rjregalado-stacktrek <126741221+rjregalado-stacktrek@users.noreply.github.com>
2023-05-12 12:16:05 +0800

    added database scripts

---
+++ database.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE api;
+
+CREATE TABLE users (
+  ID SERIAL PRIMARY KEY,
+  name VARCHAR(30),
+  email VARCHAR(30)
+);
+
+INSERT INTO users (name, email)
+VALUES ('Jerry', 'jerry@example.com'), ('George', 'george@example.com');
+
+SELECT * FROM users;
